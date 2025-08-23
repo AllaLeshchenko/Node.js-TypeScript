4b32c24d2f7857b2de6c96b7bb76beb85ea5a573
Alla Leshchenko <leshchen.a@gmail.com>
2025-08-23 12:23:05 +0200

    mini_project_1 updated

--- ClassWork/mini_project_1/middleware/authMiddleware.js
+++ ClassWork/mini_project_1/middleware/authMiddleware.js
@@ -6,7 +6,18 @@ function authToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if(!token){
-        return res.status(401).json({message: 'Access token is missing or invalid'})
+    if (!token) {
+        return res.status(401).json({ message: 'Access token is missing or invalid' });
     }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Invalid token' });
+        }
+
+        req.user = user;
+        next();
+    })
 }
+
+export default authToken;
