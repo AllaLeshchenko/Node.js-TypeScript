7bf2f886cb32152944ffef6c73a86b98dea1f39d
Alla Leshchenko <leshchen.a@gmail.com>
2025-08-28 17:43:34 +0200

    mini_project_2 updated

--- ClassWork/mini_project_2/routes/cartRoutes.js
+++ ClassWork/mini_project_2/routes/cartRoutes.js
@@ -3,6 +3,7 @@ import Product from '../models/Product.js';
 
 const router = express.Router();
 
+// Маршрут для просмотра всех продуктов в корзине
 router.get('/cart', async (req, res) => {
   try {
     const products = await Product.find();
@@ -13,6 +14,7 @@ router.get('/cart', async (req, res) => {
   }
 });
 
+// Маршрут для просмотра всех продуктов в корзине
 router.post('/cart', async (req, res) => {
   try {
     const { name, quantity, price } = req.body;
@@ -26,6 +28,43 @@ router.post('/cart', async (req, res) => {
   } catch (error) {
     console.error('Error by added product', error.message);
     res.status(400).json({ message: 'Error by added product' });
+  }
+});
+
+// Маршрут для обновления продукта в корзине
+router.put('/cart/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, quantity, price } = req.body;
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id, 
+      { name, quantity, price},
+      {new: true, runValidators: true} // Опция new: true возвращает обновленный кодумент
+    )
+    if(!updatedProduct) {
+      return res.status(404).json({message: 'Product not found'})
+    }
+    res.json(updatedProduct);
+  } catch (error) {
+    console.error('Error by updated');
+    res.status(400).json({messaage: 'Error by updated'});
+  }
+});
+
+
+// Маршрут для удаления продукта из корзины 
+router.delete('/cart/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return res.status(404).json({message: 'Product not found'})
+    }
+    res.json({message: 'Product deleted'});
+  } catch (error) {
+    res.status(500).json({message: 'Error deleting product'})
   }
 });
 
